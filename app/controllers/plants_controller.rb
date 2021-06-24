0fa95deb2f37c917bd89cd59106b5fa9331b2ef0
Lomig Enfroy <lomig@hey.com>
2021-06-24 22:33:50 +0200

    Add comments from our conversation

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,10 @@
 class PlantsController < ApplicationController
+
+  # Pas de new, car on affiche le formulaire sur la show du Garden
+  # Pas d'index, car on n'a pas de page dédiée à afficher toutes les plates
+  # Pas de show, on n'affiche pas de détail de plante (il n'y en a pas)
+  # Les plantes de chaque Garden seront affichées sur la show de ce Garden
+
   def create
     @plant = Plant.new(plant_params)
     @garden = Garden.find(params[:garden_id])
@@ -10,9 +16,10 @@ class PlantsController < ApplicationController
   end
 
   def destroy
-    @plant = Plant.find(params[:id])
-    garden = @plant.garden
-    @plant.destroy
+    plant = Plant.find(params[:id])
+    garden = plant.garden
+
+    plant.destroy
 
     redirect_to(garden_path(garden))
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :gardens do
+    # plants#create est nesté, car besoin d'un Garden pour créer une Plant
     resources :plants, only: [:create]    
   end
 
+  # plants#destroy n'est pas nesté, car pas besoin d'autre chose que SON id
   resources :plants, only: [:destroy]
 end
